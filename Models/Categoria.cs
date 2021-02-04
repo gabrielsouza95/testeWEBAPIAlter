04fb32f79540d9719935bbd93372ed60dde23827
gabrielsouza95 <alves95gabriel@gmail.com>
2021-02-03 23:20:32 -0300

    Mudado para maiúscula a primeira letra no model

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -8,17 +8,17 @@ namespace testeAlter.Models
         private const int qtde_max_name = 60, qtde_min_name = 6,
                           qtde_max_desc = 120, qtde_min_desc = 15;
         [Key]
-        public int id { get; set;}
+        public int Id { get; set;}
 
         [Required(ErrorMessage =  "Este campo é obrigatório.")]
         [MaxLength(qtde_max_name, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]
         [MinLength(qtde_min_name, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]
-        public string name { get; set;}
+        public string Name { get; set;}
 
         [Required(ErrorMessage =  "Este campo é obrigatório.")]
         [MaxLength(qtde_max_desc, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]
         [MinLength(qtde_min_desc, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]
-        public string description { get; set;}
+        public string Description { get; set;}
     }
 
 }
